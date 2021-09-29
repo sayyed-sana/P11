1ddc15de901ae99a424765d2e99c0b015a4abbec
sayyed-sana <75982511+sayyed-sana@users.noreply.github.com>
2021-09-29 15:30:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,52 @@
-var sea,ship;
-var seaImg,shipImg;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
 function preload(){
-  seaImg = loadImage("sea.png");
-  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
-                            "ship-2.png","ship-1.png");
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
+  
   createCanvas(400,400);
-  background("blue");
-
-  // Moving background
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.velocityX = -5;
-  sea.scale=0.3;
-
   
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
-  ship.scale =0.25;
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
   
+// create left Boundary
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+//create right Boundary
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
 }
 
 function draw() {
   background(0);
-  sea.velocityX = -3;
-
+  path.velocityY = 4;
+  
+  // boy moving on Xaxis with mouse
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
   
   //code to reset the background
-  if(sea.x < 0){
-    sea.x = sea.width/8;
+  if(path.y > 400 ){
+    path.y = height/2;
   }
-    
+  
   drawSprites();
 }
